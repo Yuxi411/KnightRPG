3c737ccd5d9119f6862db60fb25134e48f1f0186
Yuxi411 <162887801+Yuxi411@users.noreply.github.com>
2025-05-05 17:33:09 +0800

    敵人掉落更新

---
+++ Assets/Scripts/Items and Inventory/ItemDrop.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    [SerializeField] private int possibleItemDrops;
+    [SerializeField] private ItemData[] possibleDrop;
+    private List<ItemData> dropList = new List<ItemData>();
+
+    [SerializeField] private GameObject dropPrefab;
+    [SerializeField] private ItemData item;
+
+    public void GenerateDrop()
+    {
+        for (int i = 0; i < possibleDrop.Length; i++)
+        {
+            if (Random.Range(0, 100) <= possibleDrop[i].dropChance)
+                dropList.Add(possibleDrop[i]);
+        }
+
+        int amountOfItems = (dropList.Count < possibleItemDrops) ? dropList.Count : possibleItemDrops;
+
+        for (int i = 0; i < possibleItemDrops; i++)
+        {
+            if (dropList.Count == 0)
+                return;
+
+            ItemData randomItem = dropList[Random.Range(0, dropList.Count-1)];
+
+            dropList.Remove(randomItem);
+            DropItem(randomItem);
+        }
+    }
+
+    public void DropItem(ItemData _itemData)
+    {
+        GameObject newDrop = Instantiate(dropPrefab, transform.position, Quaternion.identity);
+
+        Vector2 randomVelocity = new Vector2(Random.Range(-5,5), Random.Range(15, 20));
+
+        newDrop.GetComponent<ItemObject>().SetupItem(_itemData,randomVelocity);
+    }
+}
